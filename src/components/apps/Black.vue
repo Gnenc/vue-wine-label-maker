05b2823855244d906c37f0602c18f84be8d0812b
Gonenc Filiz <gonenc@dakikyazilim.com>
2021-08-13 09:36:28 +0300

    Initial commit.

---
+++ src/components/apps/Black.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="black">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Black"
+}
+</script>
---
+++ src/components/apps/White.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="white">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "White"
+}
+</script>
